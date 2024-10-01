edb924686077b280f81742f3b53bd0e42211045f
unknown <anukulk2911@gmail.com>
2024-10-02 02:29:42 +0530

    first commit

---
+++ src/boards.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Boards  () {
+
+  return (
+    <div className="maindiv">
+        <div>
+            <p>Board</p>
+        </div>
+    </div>
+  );
+}
+export default Boards;
